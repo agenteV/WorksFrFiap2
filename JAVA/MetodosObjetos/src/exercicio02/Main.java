622c88996c6d34ff1011e669936a8b6e52caa3ff
agenteV <vitorfdiamantino@gmail.com>
2026-03-11 22:27:44 -0300

    exercicios de temperatura

---
+++ JAVA/MetodosObjetos/src/exercicio02/Main.java
@@ -0,0 +1,12 @@
+package exercicio02;
+
+public class Main {
+    public static void main(String[] args) {
+        MonitorEnergia monitor = new MonitorEnergia(10);
+        monitor.registrar(2);
+        monitor.registrar(1);
+        monitor.registrar(3);
+
+        System.out.println("média de consumo: " + monitor.calcularMedia());
+    }
+}
